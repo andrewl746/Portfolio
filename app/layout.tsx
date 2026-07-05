67b8b59f84d2ee842cc12df25a49b2d6ec4d32f0
Andrew Li <andrewli746@gmail.com>
2026-07-05 19:31:40 -0400

    Removed opengraph-image.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,34 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://andrewli.app"),
   title: "Andrew Li",
   description:
-    "Computer Science student at the University of Waterloo. Builder of OlympIQ and other tools that make people's lives a little easier.",
+    "Computer Science student at the University of Waterloo. Founder of OlympIQ and other tools that make people's lives easier.",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
   openGraph: {
     title: "Andrew Li",
     description:
-      "Computer Science student at the University of Waterloo. Builder of OlympIQ and other tools that make people's lives a little easier.",
+      "Computer Science student at the University of Waterloo. Founder of OlympIQ and other tools that make people's lives easier.",
     url: "https://andrewli.app",
     siteName: "Andrew Li",
+    images: [
+      {
+        url: "/icon.svg",
+        width: 64,
+        height: 64,
+        alt: "Andrew Li app icon",
+      },
+    ],
     type: "website",
+  },
+  twitter: {
+    card: "summary",
+    title: "Andrew Li",
+    description:
+      "Computer Science student at the University of Waterloo. Founder of OlympIQ and other tools that make people's lives easier.",
+    images: ["/icon.svg"],
   },
 };
 
